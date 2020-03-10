dda12510d6370f227010ad3f203e4b9d1ecfde5d
Vladislav Shulyak <vladislav.dll@gmail.com>
2020-03-10 17:16:06 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    for (let i = 0; i < bracketsConfig.length;) {
+        let searchBrackets = bracketsConfig[i].join('');
+        if (str.includes(searchBrackets)) {
+            str = str.replace(searchBrackets, '');
+            i = 0
+        } else i++
+    }
+
+    return str.length === 0;
+};
